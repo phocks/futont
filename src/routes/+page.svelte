9549eeab508149e41c5d51a2b3eadbb8fcc99d53
Joshua Byrd <phocks@gmail.com>
2024-07-17 15:19:58 +1000

    Display flex it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,29 @@
-<button class="root">Futont</button>
+<script lang="ts">
+</script>
+
+<div class="wrapper">
+	<div class="text">Futont</div>
+</div>
 
 <style lang="scss">
 	@use '../lib/mixins.scss' as *;
 
-	.root {
-		@include text-crop($top-adjustment: -0.5px, $bottom-adjustment: -2px);
+	.wrapper {
+		background-color: aquamarine;
+		height: 100px;
+		display: flex;
+		align-items: center;
+	}
+
+	.text {
 		font-size: 32px;
 		line-height: 1.3;
 		font-weight: 400;
 		font-style: normal;
-		padding: 8px;
-		background-color: aquamarine;
 		border: none;
-		margin: 0 0 0 16px;
+		@include text-crop($top-adjustment: -0.5px, $bottom-adjustment: -2px);
+		@supports (-moz-appearance: none) {
+			transform: translateY(0.045em);
+		}
 	}
 </style>
