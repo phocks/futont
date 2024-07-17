3a24d800f2e26b34a262dbfd034d6afd0aa37066
Joshua Byrd <phocks@gmail.com>
2024-07-17 10:21:34 +1000

    I don't know...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,17 @@
-<div>Futont</div>
+<button class="root">Futont</button>
 
 <style lang="scss">
-  div {
-    padding: 8px;
-    background-color: aquamarine;
-  }
+	@use '../lib/mixins.scss' as *;
+
+	.root {
+		@include text-crop($top-adjustment: -0.5px, $bottom-adjustment: -2px);
+		font-size: 32px;
+		line-height: 1.3;
+		font-weight: 400;
+		font-style: normal;
+		padding: 8px;
+		background-color: aquamarine;
+		border: none;
+		margin: 0 0 0 16px;
+	}
 </style>
